b10fbd90ccf74a39fc6e73c34cc4490c67050cd8
Yuyang Bao <baoyuyang@Yuyangs-MacBook-Pro.local>
2016-01-03 19:52:11 +0800

    admin panel added

--- photographer/admin.py
+++ photographer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Photographer, PhotographerImage
+
+class PhotographerAdmin(admin.ModelAdmin):
+	list_display = ['first_name', 'last_name', 'short_description']
+
+admin.site.register(Photographer, PhotographerAdmin)
+admin.site.register(PhotographerImage)
