1eed1ae5b73770b1353cde5dfeca8f8e07a8ff3c
Yuyang Bao <baoyuyang@Yuyangs-MacBook-Pro.local>
2016-01-13 19:58:32 +0800

    change secret setting

--- takeone/settings.py
+++ takeone/settings.py
@@ -20,7 +20,7 @@ FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u!0q&30u)d8@s83_6s%pjpivb6^axuelgld08-csxj%$#nsx_y'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
