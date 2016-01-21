e691e74e34ce7a71388acbd79562e6a6a88dbf0e
Yuyang Bao <baoyuyang@Yuyangs-MacBook-Pro.local>
2016-01-21 04:39:21 -0500

    change xml for twilio

--- sms/views.py
+++ sms/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def sms(request):
-	twiml = '<Response><Message>Hello from your Django app!</Message></Response>'
+	twiml = '<Response><Sms>Hello from your Django app!</Sms></Response>'
 	return HttpResponse(twiml, content_type='text/xml')
