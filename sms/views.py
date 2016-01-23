34855475e3e69a2a83778d4a5c7108230401fa2a
Yuyang Bao <baoyuyang@Yuyangs-MacBook-Pro.local>
2016-01-22 23:13:48 -0500

    change twilio view

--- sms/views.py
+++ sms/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 # from django.views.decorators.csrf import csrf_exempt
 from twilio.rest import TwilioRestClient 
 from django_twilio.decorators import twilio_view
@@ -17,13 +17,19 @@ def sms(request):
   msg = 'from: ' + from_city + ', ' + from_state + '\n' + 'content:\n' + content
 
   # send the msg to me and Michael
-  client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+	ACCOUNT_SID = "ACa508a195425ca7341d5469a54a91cb36" 
+	AUTH_TOKEN = "4005b7c244086f9986d6375b2b7530e3" 
+	 
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+	 
+	client.messages.create( 
+		from="+13137698688",  
+		to="+13109139124",
+		body=msg, 
+	)
 
-  client.messages.create(
-    to="+13109139124", 
-    body=msg, 
-	) 
 
   # r = Response()
   # r.message(msg)
-  return '<Response></Response>'
+	twiml = '<Response></Response>'
+  return return HttpResponse(twiml, content_type='text/xml')
