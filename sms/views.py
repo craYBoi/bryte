00716618344a6bb8c2b95188c569af415881a5d8
Yuyang Bao <baoyuyang@Yuyangs-MacBook-Pro.local>
2016-01-22 23:07:43 -0500

    change twilio view

--- sms/views.py
+++ sms/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 # from django.views.decorators.csrf import csrf_exempt
+from twilio.rest import TwilioRestClient 
 from django_twilio.decorators import twilio_view
 from twilio.twiml import Response
+from django.conf import settings
 
 # Create your views here.
 # -*- coding: utf-8 -*-
 
 @twilio_view
 def sms(request):
-  name = request.POST.get('Body', '')
-  print request.POST
-  msg = 'Hey %s, how are you today?' % (name)
-  r = Response()
-  r.message(msg)
-  return r
+  content = request.POST.get('Body', '')
+  from_city = request.POST.get('FromCity', '')
+  from_state = request.POST.get('FromState', '')
+  msg = 'from: ' + from_city + ', ' + from_state + '\n' + 'content:\n' + content
+
+  # send the msg to me and Michael
+  client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+
+  client.messages.create(
+    to="+16518675309", 
+    body=msg, 
+	) 
+
+  # r = Response()
+  # r.message(msg)
+  return '<Response></Response>'
