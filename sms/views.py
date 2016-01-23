70b4a3051bdf1f9961d32a1497578292998813c7
Yuyang Bao <baoyuyang@Yuyangs-MacBook-Pro.local>
2016-01-22 23:54:07 -0500

    change twilio view

--- sms/views.py
+++ sms/views.py
@@ -15,7 +15,8 @@ def sms(request):
 	from_number = request.POST.get('From', '')
 	content = request.POST.get('Body', '')
 
-	if from_number == settings.BEN_CELL:
+	# if from_number == settings.BEN_CELL:
+	if from_number == '+13109139124'
 		if content:
 			dest_num = content.split()[0]
 			body = content[len(dest_num)+1:]
@@ -23,7 +24,8 @@ def sms(request):
 				dest_num = '+1' + dest_num
 
 			# send the sms
-			send_msg(settings.TWILIO_CELL, dest_num, body)
+			# send_msg(settings.TWILIO_CELL, dest_num, body)
+			send_msg('+13137698688', dest_num, body)
 		else:
 			body = 'invalid format, please send again'
 			r = Response()
@@ -34,7 +36,8 @@ def sms(request):
 		from_city = request.POST.get('FromCity', '')
 		from_state = request.POST.get('FromState', '')
 		msg = 'from: ' + from_number + ', ' + from_city + ', ' + from_state + '\n' + content
-		send_msg(settings.TWILIO_CELL, settings.BEN_CELL, msg)
+		# send_msg(settings.TWILIO_CELL, settings.BEN_CELL, msg)
+		send_msg('+13137698688', '+13109139124', msg)
 
 	twiml = '<Response></Response>'
 	return HttpResponse(twiml, content_type='text/xml')
@@ -44,7 +47,8 @@ def sms(request):
 
 # helper function to send text msg
 def send_msg(input_from, input_to, input_body):
-	client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+	# client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+	client = TwilioRestClient("ACa508a195425ca7341d5469a54a91cb36", "4005b7c244086f9986d6375b2b7530e3")
 
 	client.messages.create(
 		from_= input_from,
