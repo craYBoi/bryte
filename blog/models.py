db7500f1b6da9aec10659a84436e24c4eb813f49
Yuyang Bao <baoyuyang@Yuyangs-MacBook-Pro.local>
2016-01-27 11:25:22 -0500

    add blog

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from __future__ import unicode_literals
 from django.utils.text import slugify
 from django.db import models
-from photographer.models import Photographer
 from django.core.urlresolvers import reverse
+
+from photographer.models import Photographer
+from userprofile.models import Profile
+
 
 # Create your models here.
 class Blog(models.Model):
@@ -10,6 +13,8 @@ class Blog(models.Model):
 	title = models.CharField(max_length=120)
 	content = models.TextField()
 	slug = models.SlugField(unique=True, null=True, blank=True)
+	thumbnail = models.ImageField(upload_to='blog_img/', null=True)
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
 	def __unicode__(self):
 		return self.title
@@ -21,3 +26,14 @@ class Blog(models.Model):
 	def get_absolute_url(self):
 		title = slugify(self.title)
 		return reverse("blog_detail", kwargs={"slug": self.slug})
+
+
+class Comment(models.Model):
+	blog = models.ForeignKey(Blog)
+	content = models.TextField()
+	datetime = models.DateTimeField(auto_now_add=True)
+	profile = models.ForeignKey(Profile)
+
+	def __unicode__(self):
+		return self.profile.__unicode__()
+
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.core.urlresolvers import reverse
 
 # Create your views here.
 
-from .models import Blog
+from .models import Blog, Comment
+from .forms import CommentForm
 
 class BlogListView(ListView):
 	model = Blog
 
+	def get_context_data(self, *args, **kwargs):
+		context = super(BlogListView, self).get_context_data(*args, **kwargs)
+		context['title_text'] = 'Blog Lists'
+		return context
+
 
 def BlogDetailView(request, slug):
 	blog = Blog.objects.get(slug=slug)
+	comment_form = CommentForm(request.POST or None)
+	if comment_form.is_valid() and request.user.is_authenticated():
+		instance = comment_form.save(commit=False)
+		instance.blog = blog
+		instance.profile = request.user.profile
+		instance.save()
+		return redirect(blog.get_absolute_url())
 
-	context = {}
+	comments = Comment.objects.filter(blog=blog)
+	context = {
+		'blog': blog,
+		'comments': comments,
+		'comment_form': comment_form,
+		'title_text': blog.title,
+	}
 
 	return render(request, 'blog/blog_detail.html', context)
