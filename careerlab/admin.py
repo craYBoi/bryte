4f186ac2a29c1da52e095540b1dfb39af04e541f
Yuyang Bao <baoyuyang@Yuyangs-MacBook-Pro.local>
2016-05-02 21:39:13 -0400

    add nextshoot

--- careerlab/admin.py
+++ careerlab/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Timeslot, Booking, Signup
+from .models import Timeslot, Booking, Signup, Nextshoot
 
 
 admin.site.register(Timeslot)
 admin.site.register(Booking)
 admin.site.register(Signup)
+admin.site.register(Nextshoot)
--- careerlab/models.py
+++ careerlab/models.py
@@ -2,16 +2,40 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils import timezone
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 from datetime import datetime
 from photographer.models import Photographer
 
 # Create your models here.
 
+class Nextshoot(models.Model):
+	photographer = models.ForeignKey(Photographer, related_name='nextshoot_photographer')
+	location = models.CharField(max_length=100)
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+	class Meta:
+		ordering = ('-timestamp',)
+
+	def __unicode__(self):
+		return self.location + ' - ' + self.photographer.get_full_name()
+
+	# def send_reminder(self):
+	# 	email_list = [e.email for elem in self.timeslot_set.all() for e in elem.booking_set.all()]
+	# 	try:
+	# 		send_mail('Test', 'This is the test msg', settings.EMAIL_HOST_USER, email_list, fail_silently=False)
+	# 	except Exception, e:
+	# 		print 'Email not sent'
+	# 		pass
+
+
 class Timeslot(models.Model):
 	time = models.DateTimeField()
 	is_available = models.BooleanField(default=True)
 	current_volumn = models.PositiveSmallIntegerField(default=0)
+	# shoot = models.ForeignKey(Nextshoot, default=Nextshoot.objects.first().pk)
 
 	def __unicode__(self):
 		return self.time.strftime('%m/%d/%Y %I:%M %p')
@@ -62,17 +86,3 @@ class Signup(models.Model):
 		return self.name + ' ' + self.email
 
 
-class Nextshoot(models.Model):
-	date = models.DateField()
-	photographer = models.ForeignKey(Photographer, related_name='nextshoot_photographer')
-	location = models.CharField(max_length=100)
-	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-
-	class Meta:
-		ordering = ('-timestamp',)
-
-	def __unicode__(self):
-		return str(self.date) + ' - ' + self.location + ' - ' + self.photographer.get_full_name()
-
-	def time_format(self):
-		return self.date.strftime('%B %d')
