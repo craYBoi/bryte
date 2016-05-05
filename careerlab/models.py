adeabc221d81ecc1211a56b8e0e75ce6eb04a0e6
Yuyang Bao <baoyuyang@Yuyangs-MacBook-Pro.local>
2016-05-05 15:43:24 -0400

    add dropbox send link

--- careerlab/models.py
+++ careerlab/models.py
@@ -9,6 +9,9 @@ from django.conf import settings
 from datetime import datetime
 from photographer.models import Photographer
 
+import os
+import dropbox
+
 # Create your models here.
 
 class Signup(models.Model):
@@ -37,7 +40,7 @@ class Nextshoot(models.Model):
 		for e in email_list:
 			name = e.name 
 			title = 'Your Free Headshot at CareerLAB'
-			msg = 'Hi ' + name + ',\n\nThis is a reminder that you have booked a free headshot session tomorrow, Wednesday, May, 3rd at ' + str(e.timeslot) + '.\n\nWe look forward to seeing you at the shoot! Arrive 5 minutes before your scheduled time slot is set to begin. You will have 3 minutes to take your headshot since we are fully booked for tomorrow. If you can\'t make it, please respond to this email letting us know.\n\nBest,\nCareerLAB and the Bryte Photo Team'
+			msg = 'Hi ' + name + ',\n\nThis is a reminder that you have booked a free headshot session tomorrow, Wednesday, May, 3rd at ' + str(e.timeslot) + '.\n\nWe look forward to seeing you at the shoot! Arrive 5 minutes before your scheduled time slot is set to begin. You will have 3 minutes to take your headshot since we are fully booked for tomorrow. If you can\'t make it, please respond to this email letting us know.\n\nBest,\nCareerLAB and the Bryte Photo Team.'
 			try:
 				send_mail('Your Free Headshot at CareerLAB', msg, 'Bryte Photo and CareerLAB <' + settings.EMAIL_HOST_USER + '>', [e.email], fail_silently=False)
 		# send_mail('Test', 'This is the test msg', settings.EMAIL_HOST_USER, email_list, fail_silently=False)
@@ -68,6 +71,31 @@ class Nextshoot(models.Model):
 				raise e
 			else:
 				print '[SENT] ' + email
+
+
+	def send_dropbox_links(self):
+		token = settings.DROPBOX_TOKEN
+		dbx = dropbox.Dropbox(token)
+
+		video_link = 'https://www.dropbox.com/sh/8kmxk6kvwefvkld/AADpuwmJbcjYLWE3q6TewUDha?dl=0'
+		root = '/'
+		folder = 'Deliverable'
+		name = 'CareerLAB1'
+		path = os.path.join(root, folder, name)
+		folders = dbx.files_list_folder(path)
+		for e in folders.entries:
+			email = str(e.name)
+			new_path = os.path.join(path, email)
+			sharing_link = dbx.sharing_create_shared_link(new_path, short_url=False, pending_upload=None)
+			title = 'Your headshot photo is available!'
+			msg = 'Hi!\n\nGreat news! Here is your retouched headshot photo!\n\n' + str(sharing_link.url) + '\n\nAlso, please enjoy this Promo Video we created with shots from yesterday afternoon.\n\n' + video_link +'\n\n(Let us know if you don\'t want your photo in the video.)\n\nWe\'re always looking to improve your experience with us. In the next day or two, CareerLAB will be sending out a survey about your headshots and the overall experience. Please do us a favor to fill out the survey. That will help us a lot!\n\nThank you!\nCareerLAB and Bryte Photo Team'
+			try:
+				send_mail(title, msg, 'Bryte Photo and CareerLAB <' + settings.EMAIL_HOST_USER + '>', [email], fail_silently=False)
+			except Exception, e:
+				raise e
+			else:
+				print '[SENT] ' + email
+
 
 
 
